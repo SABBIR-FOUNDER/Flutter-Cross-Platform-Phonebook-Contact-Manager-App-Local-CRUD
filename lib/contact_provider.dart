da5a6651ae70215a566836d651977d97944aa1ac
SABBIR-FOUNDER <sabbir911911@gmail.com>
2026-08-19 17:42:02 +0600

    Provider not add contact fixed and main.dart starts here

---
+++ lib/contact_provider.dart
@@ -0,0 +1,128 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:phonebook/contact_model.dart';
+import 'package:phonebook/database_helper.dart';
+
+
+class ContactProvider extends ChangeNotifier {
+  final DatabaseHelper _databaseHelper = DatabaseHelper.instance;
+
+  List<Contact> _contacts = [];
+  List<Contact> _favourites = [];
+  bool _isLoading = false;
+  String _searchQuery = '';
+
+
+  List<Contact> get favourites => _favourites;
+
+  List<Contact> get contacts => _contacts;
+
+  bool get isLoading => _isLoading;
+
+  String get searchQuery => _searchQuery;
+
+  Future<void> loadContacts() async {
+    //Load Contacts
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _contacts = await _databaseHelper.getContacts();
+      _favourites = _contacts
+          .where((contact) => contact.isFavourite)
+          .toList();
+    }
+    catch (e) {
+      debugPrint('Error Loading Contacts : $e');
+    }
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> addContact(Contact contact) async {
+    //Add  Contact
+    try {
+      await _databaseHelper.insertContact(contact);
+      await loadContacts();
+    } catch (e) {
+      debugPrint('Error Adding Contact: $e');
+    }
+  }
+
+  Future<void> updateContact(Contact contact) async {
+    //Update existing Contact
+    try {
+      await _databaseHelper.updateContact(contact);
+      await loadContacts();
+    } catch (e) {
+      debugPrint('Error updating contact: $e');
+    }
+  }
+
+  //Delete contact
+
+  Future<void> deleteConract(int id) async {
+    try {
+      await _databaseHelper.deleteContact(id);
+      await loadContacts();
+    } catch (e) {
+      debugPrint('Error Deleting Contact: $e');
+    }
+  }
+
+  Future<void> toogleFavourite(Contact contact) async {
+    // Toggle favorite/unfavourite
+    try {
+      final newFavouriteStatus = !contact.isFavourite;
+      await _databaseHelper.updateFavourite(
+        contact.id!,
+        newFavouriteStatus,
+      );
+      await loadContacts();
+    } catch (e) {
+      debugPrint('Error Updating Favourite: $e');
+    }
+  }
+
+  Future<Contact?> getContactById(int id) async {
+    // Get 1 contact by ID
+    try {
+      final contacts = await _databaseHelper.getContacts();
+      for (final contact in contacts) {
+        if (contact.id == id) {
+          return contact;
+        }
+      }
+    } catch (e) {
+      debugPrint('Error Getting Contact: $e');
+    }
+    return null;
+  }
+
+
+//Name search
+  void searchContacts(String query) {
+    _searchQuery = query.trim();
+
+    if (_searchQuery.isEmpty) {
+      loadContacts();
+      return;
+    }
+
+    final lowercaseQuery = _searchQuery.toLowerCase();
+    _contacts = _contacts.where((contact) {
+      return contact.name.toLowerCase().contains(lowercaseQuery);
+    }).toList();
+    notifyListeners();
+  }
+
+//Clear Search
+  Future<void> clearSearch() async {
+    _searchQuery = '';
+    await loadContacts();
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 
 
 
+import 'package:flutter/cupertino.dart';
+import 'package:phonebook/contact_provider.dart';
+import 'package:provider/provider.dart';
+
 void main(){
-
-
+  runApp(
+    ChangeNotifierProvider(create: (_) => ContactProvider(),
+    child: const MyApp()
+    ),
+  );
 }
+
