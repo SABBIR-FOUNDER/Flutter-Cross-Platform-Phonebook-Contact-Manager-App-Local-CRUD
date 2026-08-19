8a1cf9dd76996dfa3cbe84099fece4be97331a21
SABBIR-FOUNDER <sabbir911911@gmail.com>
2026-08-19 19:25:34 +0600

    Emulator problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,126 @@
-
-
-
-import 'package:flutter/cupertino.dart';
-import 'package:phonebook/contact_provider.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main(){
+import 'contact_provider.dart';
+
+void main() {
   runApp(
-    ChangeNotifierProvider(create: (_) => ContactProvider(),
-    child: const MyApp()
+    ChangeNotifierProvider(
+      create: (_) => ContactProvider(),
+      child: const MyApp(),
     ),
   );
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Contacts',
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepPurple,
+        brightness: Brightness.light,
+      ),
+
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      context.read<ContactProvider>().loadContacts();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Contacts'),
+        centerTitle: true,
+      ),
+
+      body: Consumer<ContactProvider>(
+        builder: (context, provider, child) {
+          if (provider.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (provider.contacts.isEmpty) {
+            return const Center(
+              child: Text(
+                'No contacts yet',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: provider.contacts.length,
+            itemBuilder: (context, index) {
+              final contact = provider.contacts[index];
+
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text(
+                    contact.name.isNotEmpty
+                        ? contact.name[0].toUpperCase()
+                        : '?',
+                  ),
+                ),
+
+                title: Text(contact.name),
+
+                subtitle: Text(contact.phone),
+
+                trailing: Icon(
+                  contact.isFavourite
+                      ? Icons.star
+                      : Icons.star_border,
+                  color: contact.isFavourite
+                      ? Colors.amber
+                      : null,
+                ),
+
+                onTap: () {
+                  debugPrint(
+                    'Selected contact: ${contact.name}',
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Add Contact button pressed');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
